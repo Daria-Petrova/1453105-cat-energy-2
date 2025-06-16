53a4dcffd9111971d4697306d0d2dac22b190863
Daria-Petrova <o.mine.God@gmail.com>
2025-06-16 22:15:34 +0300

    Сделан header для мобильной версии

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,9 @@
 /* в этот файл добавляет скрипты*/
+function toggleMenu() {
+  const btn = document.getElementById('toggle-button');
+  const menu = document.getElementById('toggle-menu');
+  btn.classList.toggle('toggle--open');
+  menu.classList.toggle('navigation-list--closed');
+}
+toggleMenu()
+
