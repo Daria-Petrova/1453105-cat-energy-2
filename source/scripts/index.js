8e3b1214772f47fd001d1a6c2e25e395eb2b27ae
Daria-Petrova <o.mine.God@gmail.com>
2025-06-23 18:19:36 +0300

    Ещё одна версия скипта

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,11 +1,7 @@
 /* в этот файл добавляет скрипты*/
-function toggleMenu() {
-  const btn = document.getElementsByClassName('toggle__button')[0];
-  const menu = document.getElementsByClassName('header__navigation-list')[0];
+document.getElementById('toggle-button').addEventListener('click', () => {
+  const btn = document.getElementById('toggle-button');
+  const menu = document.getElementById('toggle-menu');
   btn.classList.toggle('toggle--open');
   menu.classList.toggle('navigation-list--closed');
-}
-
-document.addEventListener('load', () => {
-  toggleMenu();
 });
