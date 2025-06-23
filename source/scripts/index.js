86af76c15a7e79753e9c913164e81cad79ee658b
Дарья Петрова <daria.petrova@yoso.ru>
2025-06-23 17:03:15 +0300

    правки js

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,7 +1,7 @@
 /* в этот файл добавляет скрипты*/
 function toggleMenu() {
-  const btn = document.getElementById('toggle-button');
-  const menu = document.getElementById('toggle-menu');
+  const btn = document.getElementsByClassName('toggle__button')[0];
+  const menu = document.getElementsByClassName('header__navigation-list')[0];
   btn.classList.toggle('toggle--open');
   menu.classList.toggle('navigation-list--closed');
 }
