c68967f9e204d1ee9dd9b19071888ac2817ba26f
Daria-Petrova <o.mine.God@gmail.com>
2025-07-06 10:38:36 +0300

    Поправлен header без js, переделан пример

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,10 +1,10 @@
 /* в этот файл добавляет скрипты*/
 window.addEventListener('DOMContentLoaded', () => {
-  document.querySelector('.header__navigation--js').classList.remove('header__navigation--hidden');
+  document.querySelector('.header__navigation').classList.remove('header__navigation--none-js');
 });
 
-document.getElementById('toggle-button').addEventListener('click', () => {
-  const btn = document.getElementById('toggle-button');
+document.querySelector('.toggle__button').addEventListener('click', () => {
+  const btn = document.querySelector('.toggle__button');
   const menu = document.getElementById('toggle-menu');
   btn.classList.toggle('toggle--open');
   menu.classList.toggle('navigation-list--closed');
